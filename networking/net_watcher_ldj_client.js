bc060c6b6d3a02c5445c2dc9d8d9bc836e84e718
nwoodard-cs <nwoodard.cs@gmail.com>
2018-10-04 10:35:24 -0600

    Ldj module to handle message fragmentation

---
+++ networking/net_watcher_ldj_client.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const netClient = require('net').connect({ port: 60300 })
+const ldjClient = require('./lib/ldj_client.js').connect(netClient)
+
+ldjClient.on('message', msg => {
+    if (msg.type === 'watching')
+        console.log(`Now watching ${msg.file}`)
+    else if (msg.type === 'changed')
+        console.log(`File changed on ${new Date(msg.timestamp)}`)
+    else throw Error(`Unknown message type ${msg.type}`)
+})
