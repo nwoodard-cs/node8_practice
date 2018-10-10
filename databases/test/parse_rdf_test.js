d9b6786704622d8cf11c981229632ed04c98cabf
nwoodard-cs <nwoodard.cs@gmail.com>
2018-10-10 14:56:47 -0600

    Attribute tests and individual RDF to JSON functionality

--- databases/test/parse_rdf_test.js
+++ databases/test/parse_rdf_test.js
@@ -14,6 +14,19 @@ describe('parseRDF', () => {
 
     it('should parse RDF content', () => {
         const book = parseRDF(rdf)
+
         expect(book).to.be.an('object')
+        expect(book).to.have.a.property('id', 132)
+        expect(book).to.have.a.property('title')
+
+        expect(book).to.have.a.property('authors')
+         .that.is.an('array').with.lengthOf(2)
+         .and.contains('Sunzi, active 6th century B.C.')
+         .and.contains('Giles, Lionel')
+
+        expect(book).to.have.a.property('subjects')
+         .that.is.an('array').with.lengthOf(2)
+         .and.contains('Military art and science -- Early works to 1800')
+         .and.contains('War -- Early works to 1800')
     })
 })
